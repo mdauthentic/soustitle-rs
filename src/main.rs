b096816a3fac1dfdeee727cef21085ba2801a61d
Muideen Lawal <mdauthentic@Mdauthentic-MacBook-Pro.local>
2022-09-24 01:15:13 +0200

    add clap for cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
+use clap::{Arg, App};
 mod lib;
 
 fn main() {
 
-    let result = lib::parse("/data/sample.srt");
-    println!("Parsed SRT: {:?}", result);
+    // Usage: 
+    //      cargo run -- -i path/data/sample.srt
+
+    let matches = App::new("Soustitle-rs")
+        .version("0.1.0")
+        .author("Muideen Lawal <muideen.lawal320@gmail.com>")
+        .about("A simple (subtitle) .srt parser written in rust.")
+        .arg(Arg::with_name("input")
+                 .short('i')
+                 .long("input")
+                 .takes_value(true)
+                 .help("Path to an .srt file"))
+        .arg(Arg::with_name("output")
+                 .short('o')
+                 .long("output")
+                 .takes_value(true)
+                 .help("Path to save the output file"))
+        .get_matches();
+
+        let myfile = matches.value_of("input");
+        match myfile {
+            None => println!("No idea what your favorite number is."),
+            Some(s) => {
+                let result = lib::parse(s);
+                println!("Parsed SRT: {:?}", result);
+            }
+        }
+
+    // let result = lib::parse("/data/sample.srt");
+    // println!("Parsed SRT: {:?}", result);
 
     //lib::write_data(result, "/data/output.csv", true).unwrap();
 
